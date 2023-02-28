5edfd201b780c453d5593b49dc5f0da13d215e4a
gustavo21332 <pindamom54@gmail.com>
2023-02-28 14:15:57 +0000

    salvar

--- Tipos-de-dados-java-main/Atividade1.java
+++ Tipos-de-dados-java-main/Atividade1.java
@@ -5,13 +5,17 @@ public class Atividade1 {
     public static void main(String[]args){
     Scanner input = new Scanner(System.in);
     int num;
-    System.out.println("informe o valor da variável");
-    num = input.nextInt();
 
-    if(num >= 0){
-        System.out.print("Positivo");
-    } 
-    else
-    {System.out.print( "Negativo");}
+        System.out.println("informe o valor da variável");
+        num = input.nextInt();
+
+        if(num > 0){
+            System.out.print("Positivo");
+        } 
+        else if (num < 0)
+        {System.out.print( "Negativo");}
+        else{
+            System.out.print( "numero é 0");
+        }
     }
 }
