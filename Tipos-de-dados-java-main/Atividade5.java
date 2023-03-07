0fb24c94e3eca7412d61bfdf396561357040f4e4
gustavo21332 <pindamom54@gmail.com>
2023-03-07 14:51:00 +0000

    Salvar

--- Tipos-de-dados-java-main/Atividade5.java
+++ Tipos-de-dados-java-main/Atividade5.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Atividade5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Double M,C;
+
+        System.out.println("informe a quantidade de metros");
+        M = input.nextDouble();
+
+       C = M * 100;
+
+       System.out.println("Convertendo para centímetros seria:" + C);
+
+
+
+    }
     
 }
